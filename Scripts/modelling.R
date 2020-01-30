06e2a117f1c19c50560e7d5491fb642f894bd3b3
BregoSrego <59478540+BregoSrego@users.noreply.github.com>
2020-01-30 09:44:56 +0100

    Add files via upload

---
+++ Scripts/modelling.R
@@ -0,0 +1,24 @@
+set.seed(123)
+
+#Split data
+ind1 = createDataPartition(data$Class, p=0.7, list=FALSE)
+train1 <- data[ind1,]
+test1 <- data[-ind1,]
+
+#data modelling
+model1 <- lapply(c( 'cforest', 'ctree', 'xgbTree', 'rf'),
+                              function (met) {
+                                caret::train(Class~., method = met,
+                                data=train1) })
+
+#leave one out cross validation
+train_cont <- trainControl(method = "LOOCV")
+
+#data modelling
+
+model2 <- lapply(c( 'cforest', 'ctree', 'xgbTree', 'rf'),
+                 function (met) {
+                   caret::train(Class~., data=data, method=met, 
+                                trControl = train_cont) })
+
+ 
